43affa4f5da6c3f02bff4bb7ef32cc38135e299e
Nabeil Lemar <nabeillemar@gmail.com>
2020-12-12 22:55:37 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,8 +2,13 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid #defining our Kid class 
+  include Dance #this allows us to led all methods from the Dance module to the Kid Class 
   
+  attr_accessor :name 
   
+  def initialize(name)
+    @name = name 
+  end 
   
   
   
