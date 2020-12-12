c9a098c14d5162e4e16cbc487a441b8974e49198
Nabeil Lemar <nabeillemar@gmail.com>
2020-12-12 23:04:51 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,6 +4,8 @@ require_relative './dance_module.rb'
 class Dancer #defining our class
   include Dance #the include keyword allows our classes ti use all the methods of the included module as instance methods 
   extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
 attr_accessor :name 
 
