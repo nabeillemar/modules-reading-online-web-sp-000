8fae568a7244ccba33705aa4ba81e46482ea3ccc
Nabeil Lemar <nabeillemar@gmail.com>
2020-12-12 23:05:54 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,8 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
   
 class Kid #defining our Kid class 
-  include Dance #this allows us to led all methods from the Dance module to the Kid Class 
-  extend MetaDancing 
+  #include Dance #this allows us to led all methods from the Dance module to the Kid Class 
+  #extend MetaDancing 
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
   
