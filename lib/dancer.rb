c4657d01f1b006952ee13ca2bfdef004b771455d
Nabeil Lemar <nabeillemar@gmail.com>
2020-12-12 22:56:55 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,8 +1,8 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Dancer #defining our class 
-include Dance 
+class Dancer #defining our class
+  include Dance 
 
 attr_accessor :name 
 
