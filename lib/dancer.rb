049a75d6a9a3816aaa4194a7ea37ac21ce069537
Nabeil Lemar <nabeillemar@gmail.com>
2020-12-12 23:07:01 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,5 +1,6 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
 
 class Dancer #defining our class
   #include Dance #the include keyword allows our classes ti use all the methods of the included module as instance methods 
